f4a6dd4bb16c8d547210aa6b64ba07bf193876af
emekoba <70672283+emekoba@users.noreply.github.com>
2021-12-03 10:47:22 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  // await app.listen(process.env.PORT || 9200);
-  await app.listen(9200);
+  await app.listen(process.env.PORT || 9200);
+  // await app.listen(9200);
 }
 bootstrap();
